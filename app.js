54a65689b5033944153aad53a2ad44f5d6f090b7
Amanrj064 <kumarmohit54751@gmail.com>
2025-06-22 12:12:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const nextTopperRoutes = require('./routes/nextTopper');
+const nextTopperRoutes = require('./src/routes/nextTopper');
 
 const app = express();
 
