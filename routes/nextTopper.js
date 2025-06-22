6bf023adcd7531e504a3a0e993432e6078f133b7
Amanrj064 <kumarmohit54751@gmail.com>
2025-06-22 12:07:45 +0530

    Add files via upload

---
+++ routes/nextTopper.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const loadContent = () => {
+  const filePath = path.join(__dirname, '..', '..', 'content.json');
+  if (fs.existsSync(filePath)) {
+    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+  }
+  return {};
+};
+
+router.get('/', (req, res) => {
+  res.send('Next Topper Main Page');
+});
+
+// Dynamic routes for math
+router.get('/math/:topic', (req, res) => {
+  const data = loadContent();
+  const topic = req.params.topic;
+  const content = data.math?.[topic] || [];
+  res.render(`nextTopper/math/${topic}`, { content });
+});
+
+// Dynamic routes for science
+router.get('/science/:topic', (req, res) => {
+  const data = loadContent();
+  const topic = req.params.topic;
+  const content = data.science?.[topic] || [];
+  res.render(`nextTopper/science/${topic}`, { content });
+});
+
+// Dynamic routes for s_st
+router.get('/s_st/:topic', (req, res) => {
+  const data = loadContent();
+  const topic = req.params.topic;
+  const content = data.s_st?.[topic] || [];
+  res.render(`nextTopper/s_st/${topic}`, { content });
+});
+
+module.exports = router;
